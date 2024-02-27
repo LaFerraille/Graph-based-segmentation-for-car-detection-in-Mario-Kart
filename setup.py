494454bb21716f030f63339f77814431a44eea92
Raphaël Romand-Ferroni <raphael.romandferroni@student-cs.fr>
2024-02-27 20:54:37 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Graph Cut Algorithm: Implement a graph cut algorithm tailored for segmenting bananas in Mario Kart images. Explore variations such as max-flow/min-cut algorithms for efficient segmentation.',
+    author='FERRONI-ROVERATO-GOPEE',
+    license='MIT',
+)
