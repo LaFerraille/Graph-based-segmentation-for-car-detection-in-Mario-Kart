96b0a2fd20f8437d3c3446c6f858c54d9b57779f
Quentin Gopée <quentin.gopee@student-cs.fr>
2024-03-26 15:31:57 +0100

    clean main

--- BoykovKolmogorov/main.py
+++ BoykovKolmogorov/main.py
@@ -22,30 +22,28 @@ def main():
 			inputfile = arg
 	print('Using image: ', inputfile)
 
-	I=cv2.imread(inputfile) #imread wont rise exceptions by default
-	I_dummy=np.zeros(I.shape)
-	I_dummy=np.copy(I)
+	image = cv2.imread(inputfile)
 	
 	GUI = GUI_seeds(inputfile)
 	marked_ob_pixels, marked_bg_pixels = GUI.labelling()
 
-	G, s, t, I_marked, sp_lab = img_to_graph(I, marked_ob_pixels, marked_bg_pixels)
+	G, s, t, I_marked, sp_lab = img_to_graph(image, marked_ob_pixels, marked_bg_pixels)
 	G_residual = BoykovKolmorogov(G, s, t, capacity='sim').max_flow()
 	
-	h,w,c=I.shape
+	h , w, _ = image.shape
 	St, _ = G_residual.graph['trees']
 	partition = (set(St), set(G) - set(St))
 	F=np.zeros((h,w),dtype=np.uint8)
 	for sp in partition[0]:
 		for pixels in sp.pixels:
-			i,j=pixels
-			F[i][j]=1
-	Final=cv2.bitwise_and(I,I,mask = F)
+			i, j = pixels
+			F[i][j] = 1
+	Final = cv2.bitwise_and(image, image, mask=F)
 
 
 	plt.subplot(2,2,1)
 	plt.tick_params(labelcolor='black', top='off', bottom='off', left='off', right='off')
-	plt.imshow(I[...,::-1])
+	plt.imshow(image[...,::-1])
 	plt.axis("off")
 	plt.xlabel("Input image")
 
@@ -60,12 +58,10 @@ def main():
 	plt.axis("off")
 	plt.xlabel("Super-pixel representation")
 
-
 	plt.subplot(2,2,4)
 	plt.imshow(Final[...,::-1])
 	plt.axis("off")
 	plt.xlabel("Output Image")
-
 	
 	cv2.imwrite("out.png",Final)
 	plt.show()
